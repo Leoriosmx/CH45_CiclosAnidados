d6ae88eeee952c19b1456d4b4c098e875b7b9b91
leomx <riosdeleomx@gmail.com>
2024-09-11 21:12:42 -0600

    Se modifico el .gitignore y se suben los ejercicios

---
+++ src/Triangulo.java
@@ -0,0 +1,13 @@
+
+public class Triangulo {
+
+	public static void main(String[] args) {
+
+		for (int x = 5; x > 0; x--) {
+		    for (int y = 5; y > x; y--) {
+		       System.out.print("*");
+		  }//for x
+		   System.out.println("*");
+		}//for y
+	}//main
+}//class Triangulo
---
+++ src/TrianguloInvertido.java
@@ -0,0 +1,9 @@
+
+public class TrianguloInvertido {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
